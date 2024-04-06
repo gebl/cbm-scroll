3e59795bb393878a7e5be6b7621af027bd564175
Gabriel Lawrence <gabriel.lawrence@gmail.com>
2024-04-06 15:43:48 -0500

    lives/score + barrels moving + jump

--- main.c
+++ main.c
@@ -9,9 +9,24 @@
 #include "blt.h"
 #include "game.h"
 
-void bgcolor(unsigned char color) { POKE(53280, color); }
+#define BSTART 239
+#define LIVESSTART 0
+#define LIVESWIDTH 26
+#define LIVESY 50
 
-void bordercolor(unsigned char color) { POKE(53281, color); }
+#define GROUND 8 * 13 + 50 + 4
+
+#define DINOX 130
+#define JUMPCLEAR GROUND - 20
+typedef struct viz_struct {
+    unsigned x0 : 1;
+    unsigned x1 : 1;
+    unsigned x2 : 1;
+} viz_struct;
+
+void bordercolor(unsigned char color) { POKE(53280, color); }
+
+void bgcolor(unsigned char color) { POKE(53281, color); }
 
 void initPat() {
     unsigned int i;
@@ -28,19 +43,42 @@ void fillSit(unsigned int frame, unsigned int row) {
     blt();
 }
 
+void initColorTable() {
+    unsigned int i, j;
+
+    for (i = 0; i < 16; i++) {
+        for (j = 0; j < 40; j++) {
+            outb((unsigned char *)COLOR_RAM + i * 40 + j, COLOR_BLUE);
+        }
+    }
+
+    for (i = 16; i < 24; i++) {
+        for (j = 0; j < 40; j++) {
+            outb((unsigned char *)COLOR_RAM + i * 40 + j, COLOR_BROWN);
+        }
+    }
+}
+
 unsigned int game() {
     unsigned int frame;
 
     unsigned char *temp;
     unsigned int temppage;
 
-    unsigned int score;
+    unsigned int score, score2;
     unsigned char block;
     unsigned char v;
     unsigned int i, j;
+    int yv = 0;
+    unsigned char key, lastkey, lastjump, lives;
+    viz_struct viz;
 
     score = 0;
+    score2 = 0;
+    lives = 3;
+    lastjump = 0;
     v = inb(&VIC.addr);
+    key = 0;
 
     curbuffer = SCREEN1;
     curpage = PAGE1;
@@ -57,40 +95,194 @@ unsigned int game() {
         }
     }
 
-    for (i=0xD800; i<0xDBFF; i++) {
-        outb((unsigned char *)i, COLOR_GREEN);
-    }
+    initColorTable();
+    // outb(&CIA2.pra, (block & 0xFC) | (((unsigned int)curbuffer >> 14) ^
+    // 0x03)); // video ram at $c000-$ffff
 
+    /**
+     * $DD00 = %xxxxxx11 -> bank0: $0000-$3fff
+     * $DD00 = %xxxxxx10 -> bank1: $4000-$7fff
+     * $DD00 = %xxxxxx01 -> bank2: $8000-$bfff
+     * $DD00 = %xxxxxx00 -> bank3: $c000-$ffff
+     *
+     * https://codebase64.org/doku.php?id=base:vicii_memory_organizing
+     */
     block = inb(&CIA2.pra);
-    outb(&CIA2.pra, (block & 0xFC) | (((unsigned int)curbuffer >> 14) ^ 0x03));
+    outb(&CIA2.pra,
+         block & 0xfffc);  // video ram at $c000-$ffff - 3	xxxxxx00 in DD00
     outb(&VIC.addr, curpage);
+
+    // dinosaur
+    outb((unsigned char *)SCREEN1 + SPRITE0, SPRITE0_PTR);
+    outb((unsigned char *)SCREEN2 + SPRITE0, SPRITE1_PTR);
+
+    // barrel1
+    outb((unsigned char *)SCREEN1 + SPRITE1, SPRITE2_PTR);
+    outb((unsigned char *)SCREEN2 + SPRITE1, SPRITE3_PTR);
+    // barrel2
+    outb((unsigned char *)SCREEN1 + SPRITE2, SPRITE3_PTR);
+    outb((unsigned char *)SCREEN2 + SPRITE2, SPRITE2_PTR);
+    // barrel3
+    outb((unsigned char *)SCREEN1 + SPRITE3, SPRITE2_PTR);
+    outb((unsigned char *)SCREEN2 + SPRITE3, SPRITE3_PTR);
+
+    // Live 1
+    outb((unsigned char *)SCREEN1 + SPRITE4, SPRITE0_PTR);
+    outb((unsigned char *)SCREEN2 + SPRITE4, SPRITE1_PTR);
+
+    // Live 2
+    outb((unsigned char *)SCREEN1 + SPRITE5, SPRITE0_PTR);
+    outb((unsigned char *)SCREEN2 + SPRITE5, SPRITE1_PTR);
+    // Live 3
+    outb((unsigned char *)SCREEN1 + SPRITE6, SPRITE0_PTR);
+    outb((unsigned char *)SCREEN2 + SPRITE6, SPRITE1_PTR);
+
+    for (i = 0; i < 64; i++) {
+        outb((unsigned char *)BANK + (SPRITE0_PTR * 64) + i, dino1[i]);
+    }
+    for (i = 0; i < 64; i++) {
+        outb((unsigned char *)BANK + (SPRITE1_PTR * 64) + i, dino2[i]);
+    }
+    for (i = 0; i < 64; i++) {
+        outb((unsigned char *)BANK + (SPRITE2_PTR * 64) + i, barrel1[i]);
+    }
+    for (i = 0; i < 64; i++) {
+        outb((unsigned char *)BANK + (SPRITE3_PTR * 64) + i, barrel2[i]);
+    }
 
     initPat();
     outb(XSCROLL, inb(XSCROLL) & 0xF8 & 0xf7);
-    while (1) {
+
+    VIC.spr_ena = 0x7f;
+    VIC.spr0_x = DINOX;
+    VIC.spr0_y = GROUND;
+    VIC.spr0_color = COLOR_WHITE;
+
+    VIC.spr1_x = BSTART;
+    VIC.spr1_y = GROUND;
+
+    VIC.spr2_x = BSTART;
+    VIC.spr2_y = GROUND;
+
+    VIC.spr3_x = BSTART;
+    VIC.spr3_y = GROUND;
+
+    VIC.spr4_x = LIVESSTART +2*LIVESWIDTH;
+    VIC.spr4_y = LIVESY;
+
+    VIC.spr5_x = LIVESSTART + LIVESWIDTH;
+    VIC.spr5_y = LIVESY;
+
+    VIC.spr6_x = LIVESSTART;
+    VIC.spr6_y = LIVESY;
+
+    VIC.spr_hi_x = 0x70;
+    VIC.spr0_color = COLOR_WHITE;
+    VIC.spr1_color = COLOR_BROWN;
+    VIC.spr2_color = COLOR_BROWN;
+    VIC.spr3_color = COLOR_BROWN;
+    VIC.spr4_color = COLOR_WHITE;
+    VIC.spr5_color = COLOR_WHITE;
+    VIC.spr6_color = COLOR_WHITE;
+
+    viz.x0 = 1;
+    lives=3;
+
+    while (lives>0) {
         // waitvsync();
         score++;
 
         if (score % 8 == 0) {
             frame++;
-            frame %= (MAPCOLS-40);
+            frame %= (MAPCOLS - 40);
             temp = curbuffer;
             curbuffer = backbuffer;
             backbuffer = temp;
             temppage = curpage;
             curpage = backpage;
             backpage = temppage;
-
-            block = inb(&CIA2.pra);
-            outb(&CIA2.pra,
-                 (block & 0xFC) | (((unsigned int)curbuffer >> 14) ^ 0x03));
             outb(&VIC.addr, curpage);
         }
         outb(XSCROLL, (inb(XSCROLL) & 0xF8) + (7 - score % 8));
-    
-        fillSit(frame, (7 - score % 8)*3);
-        fillSit(frame, (7 - score % 8)*3+1);
-        fillSit(frame, (7 - score % 8)*3+2);
+
+        fillSit(frame, (7 - score % 8) * 3);
+        fillSit(frame, (7 - score % 8) * 3 + 1);
+        fillSit(frame, (7 - score % 8) * 3 + 2);
+
+        if (VIC.spr0_y < GROUND) {
+            yv = yv + 1;
+        } else {
+            VIC.spr0_y = GROUND;
+            yv = 0;
+            lastjump = 0;
+        }
+
+        key = cbm_k_getin();
+        if (key == ' ') {
+            if (yv == 0 && lastjump == 0) {
+                yv = -35;
+            } else if (lastjump < 2) {
+                lastjump++;
+                yv = -30;
+            }
+        }
+        lastkey = key;
+
+        VIC.spr0_y += (yv / 8);
+
+        if (VIC.spr0_y < 50) {
+            VIC.spr0_y = 50;
+        }
+
+        if (viz.x0) {
+            VIC.spr1_x -= 1;
+            if (VIC.spr1_x > DINOX - 10 && VIC.spr1_x < DINOX + 10 &&
+                VIC.spr0_y > JUMPCLEAR) {
+                lives--;
+                VIC.spr_ena=VIC.spr_ena>>1;
+                VIC.spr1_x = BSTART;
+            }
+            if (VIC.spr1_x < 5) {
+                VIC.spr1_x = BSTART;
+                score2 = score2 + 2;
+            }
+        }
+        if (viz.x1) {
+            VIC.spr2_x -= 1;
+            if (VIC.spr2_x > DINOX - 10 && VIC.spr2_x < DINOX + 10 &&
+                VIC.spr0_y > JUMPCLEAR) {
+                viz.x1 = 0;
+                lives--;
+                VIC.spr_ena=VIC.spr_ena>>1;
+                VIC.spr2_x = BSTART;
+            } else if (VIC.spr2_x < 5) {
+                viz.x1 = 0;
+                VIC.spr2_x = BSTART;
+                score2 = score2 + 20;
+            }
+        } else {
+            if (score % 500 == 0) {
+                viz.x1 = 1;
+            }
+        }
+        if (viz.x2) {
+            VIC.spr3_x -= 1;
+            if (VIC.spr3_x > DINOX - 10 && VIC.spr3_x < DINOX + 10 &&
+                VIC.spr0_y > JUMPCLEAR) {
+                viz.x2 = 0;
+                lives--;
+                VIC.spr_ena=VIC.spr_ena>>1;
+                VIC.spr3_x = BSTART;
+            } else if (VIC.spr3_x < 5) {
+                viz.x2 = 0;
+                VIC.spr3_x = BSTART;
+                score2 = score2 + 10;
+            }
+        } else {
+            if (score % 350 == 0) {
+                viz.x2 = 1;
+            }
+        }
     }
 
     outb(&VIC.addr, v);
@@ -101,7 +293,7 @@ unsigned int game() {
 
 int main() {
     unsigned int score;
-    bgcolor(COLOR_BLACK);
+    bgcolor(COLOR_LIGHTGREEN);
     bordercolor(COLOR_BLACK);
     while (1) {
         score = game();
--- main.h
+++ main.h
@@ -1,18 +1,144 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+/**
+ * The first important thing is that the VICII can only adress 16k ram at once.
+ * This means that the 64k memory is divided into four 16k VIC banks. $DD00's
+ * lowmost 2 bits controls that which bank is seen by the VIC:
+ *
+ * $DD00 = %xxxxxx11 -> bank0: $0000-$3fff
+ * $DD00 = %xxxxxx10 -> bank1: $4000-$7fff
+ * $DD00 = %xxxxxx01 -> bank2: $8000-$bfff
+ * $DD00 = %xxxxxx00 -> bank3: $c000-$ffff
+ *
+ * If speed is not critical its generally a good idea to change vic bank like
+ * this:
+ *
+ * lda $DD00
+ * and #%11111100
+ * ora #%000000xx ;<- your desired VIC bank value, see above
+ * sta $DD00
+ *
+ * These are controlled by $D018.
+ * The Screen dimensions are 40×25 = 1000 bytes of data. When telling the C64
+ * where you want to put your screen data, this is rounded up to 1024 = $400, so
+ * you can put screen data on each multiple of $400 bytes in memory. $0400,
+ * $0800, $0c00, $1000, $1400, and so on.
+ *
+ * Char mem is made up of 256x8 byte chars: 256*8= 2048 -> $0800
+ * Bitmap is (40x8)x25 = 8000, rounded up to 8192 -> $2000
+ *
+ * $D018/53272/VIC+24:   Memory Control Register
+ * +----------+---------------------------------------------------+
+ * | Bits 7-4 |   Video Matrix Base Address (inside VIC)          |
+ * | Bit  3   |   Bitmap-Mode: Select Base Address (inside VIC)   |
+ * | Bits 3-1 |   Character Dot-Data Base Address (inside VIC)    |
+ * | Bit  0   |   Unused                                          |
+ * +----------+---------------------------------------------------+
+ *
+ *
+ * Bitmap
+ * $D018 = %xxxx0xxx -> bitmap is at $0000
+ * $D018 = %xxxx1xxx -> bitmap is at $2000
+ *
+ *
+ * Character memory
+ * $D018 = %xxxx000x -> charmem is at $0000
+ * $D018 = %xxxx001x -> charmem is at $0800
+ * $D018 = %xxxx010x -> charmem is at $1000
+ * $D018 = %xxxx011x -> charmem is at $1800
+ * $D018 = %xxxx100x -> charmem is at $2000
+ * $D018 = %xxxx101x -> charmem is at $2800
+ * $D018 = %xxxx110x -> charmem is at $3000
+ * $D018 = %xxxx111x -> charmem is at $3800
+ *
+ *
+ * Screen memory
+ * $D018 = %0000xxxx -> screenmem is at $0000
+ * $D018 = %0001xxxx -> screenmem is at $0400
+ * $D018 = %0010xxxx -> screenmem is at $0800
+ * $D018 = %0011xxxx -> screenmem is at $0c00
+ * $D018 = %0100xxxx -> screenmem is at $1000
+ * $D018 = %0101xxxx -> screenmem is at $1400
+ * $D018 = %0110xxxx -> screenmem is at $1800
+ * $D018 = %0111xxxx -> screenmem is at $1c00
+ * $D018 = %1000xxxx -> screenmem is at $2000
+ * $D018 = %1001xxxx -> screenmem is at $2400
+ * $D018 = %1010xxxx -> screenmem is at $2800
+ * $D018 = %1011xxxx -> screenmem is at $2c00
+ * $D018 = %1100xxxx -> screenmem is at $3000
+ * $D018 = %1101xxxx -> screenmem is at $3400
+ * $D018 = %1110xxxx -> screenmem is at $3800
+ * $D018 = %1111xxxx -> screenmem is at $3c00
+ * 
+ * 
+ * Sprites
+ * Sprites read their data based on the value of their corresponding Sprite Pointer. Sprite Pointers are located always at the _given_ screen memory's last 8 bytes. ie: screen_memory+$03f8 = sprite pointer0. As everything sprite adresses are also _relative_ to the start of the VIC bank.
+ * 
+ * for dummies:
+ * screen_memory_start+$03f8 = sprite pointer0
+ * screen_memory_start+$03f9 = sprite pointer1
+ * screen_memory_start+$03fa = sprite pointer2
+ * screen_memory_start+$03fb = sprite pointer3
+ * screen_memory_start+$03fc = sprite pointer4
+ * screen_memory_start+$03fd = sprite pointer5
+ * screen_memory_start+$03fe = sprite pointer6
+ * screen_memory_start+$03ff = sprite pointer7
+ * 
+ *   https://codebase64.org/doku.php?id=base:vicii_memory_organizing
+ * 
+ * 
+ */
+#define BANK 0xc000
 #define BUFFER (unsigned char *)0x0400
 #define SCREEN1 (unsigned char *)0xE000
 #define SCREEN2 (unsigned char *)0xE400
 #define CHARSET (unsigned char *)0xE800
-#define COLORRAM (unsigned char *)0xD800
+
+#define SPRITE0_PTR 0x00
+#define SPRITE1_PTR 0x01
+#define SPRITE2_PTR 0x02
+#define SPRITE3_PTR 0x03
+#define SPRITE3_PTR 0x04
+#define SPRITE3_PTR 0x05
+#define SPRITE3_PTR 0x06
+#define SPRITE3_PTR 0x07
+
+
+
+#define SPRITE0 0x03F8
+#define SPRITE1 0x03F9
+#define SPRITE2 0x03FA
+#define SPRITE3 0x03FB
+#define SPRITE4 0x03FC
+#define SPRITE5 0x03FD
+#define SPRITE6 0x03FE
+#define SPRITE7 0x03FF
+
 #define XSCROLL (unsigned char *)0xD016
 #define outb(addr, val) (*(addr) = (val))
 #define inb(addr) (*(addr))
 
-/* Values for the VIC address register to switch between the two pages */
-#define PAGE1 (((unsigned int)SCREEN1 >> 6) & 0xF0) | (((unsigned int)CHARSET >> 10) & 0x0E)
-#define PAGE2 (((unsigned int)SCREEN2 >> 6) & 0xF0) | (((unsigned int)CHARSET >> 10) & 0x0E)
+/** Values for the VIC address register to switch between the two pages
+ * Shift address right 6 bits and mask with 0xF0 to get the high bits from the
+ * address into the high 4 bits of the address register. This is used to allow
+ * the defines above of what pages of video ram are being used to be dynamically
+ * calculated.
+ *
+ * This is then ORed with the high bits of the charset address shifted right 10,
+ * the three bits select where the charset is in memory. This is masked with
+ * 0x0e to remove the least significant bit which is undefined.
+ *
+ * TODO: For perfomance reasons, this should prebably be statically set instead.
+ *
+ *
+ */
+#define PAGE1                               \
+    (((unsigned int)SCREEN1 >> 6) & 0xF0) | \
+        (((unsigned int)CHARSET >> 10) & 0x0E)
+#define PAGE2                               \
+    (((unsigned int)SCREEN2 >> 6) & 0xF0) | \
+        (((unsigned int)CHARSET >> 10) & 0x0E)
 
 #define WRAP ((MAPCOLS - 32) * 8)
 
